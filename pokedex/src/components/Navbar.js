483851f61301e978b5c5e8f3b8375b3fdc484001
Hugo Guiomar <82777546+HugoSilva4@users.noreply.github.com>
2023-04-21 21:20:15 +0200

    Navbar

---
+++ pokedex/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img
+      alt="pokémon-logo" 
+      src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"/>
+    </nav>
+  );
+};
+
+export default Navbar;

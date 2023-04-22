816faa77126fd9a86d259f9ed2d76cf4143f16c0
daviskyLV <ledaviskylv@gmail.com>
2023-04-22 14:27:44 +0200

    Router thingy

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navbar />
       <div className="App"></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={ <Navbar/> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
